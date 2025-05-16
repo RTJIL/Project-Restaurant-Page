6e401c250819ff46007fa4553b92df71e1b46950
Artem <161304626+RTJIL@users.noreply.github.com>
2025-05-16 16:27:40 +0000

    add home page

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+// webpack.config.js
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource", // tells Webpack to emit the file to output dir
+        generator: {
+          filename: "fonts/[name][ext]", // optional: put fonts in a fonts folder in dist
+        },
+      },
+    ],
+  },
+}
